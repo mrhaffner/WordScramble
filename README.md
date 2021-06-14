c9a23940681b2964cc7efda7e2e13ed51884eafc
mrhaffner <mrhaffner13@gmail.com>
2021-06-14 14:45:51 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the WordScramble partial code along project from 100 days of SwiftUI.  WordScramble is a simple iOS game that challenges you to find words within a word.
---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Matt Haffner on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
